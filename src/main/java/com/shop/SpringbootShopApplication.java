1c575e9c930dee0b06787f495de1c981eaa37693
jiyo01 <jyyy0219@gmail.com>
2023-01-27 20:10:30 +0900

    MySQL 연결 설정, JPA 옵션 추가

--- src/main/java/com/shop/SpringbootShopApplication.java
+++ src/main/java/com/shop/SpringbootShopApplication.java
@@ -1,4 +1,4 @@
-package com.shop.springbootshop;
+package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class SpringbootShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootShopApplication.class, args);
 	}
-
 }
